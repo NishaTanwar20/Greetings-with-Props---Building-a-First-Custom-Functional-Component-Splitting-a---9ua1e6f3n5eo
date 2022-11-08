e4b7997a1df0561a14f9f66dd4bd0949e2dab3d4
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-08 08:42:00 +0530

    change in text

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ export default function Welcome(props){
     return (
        
         <React.Fragment>
-            Hey !<h1>{props.name}</h1>
+           <h1>"Hey !"{props.name}</h1>
             <h2>"Welcome to Newton School."</h2>
         </React.Fragment>
     );
