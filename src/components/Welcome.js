899866e07a82afaef0c234a176765a33c96f8f9b
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-10 19:15:33 +0530

    prop changed

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,10 @@
 // write code for Welcome component here
 import React from 'react';
-export default function Welcome(props){
+export default function Welcome(name){
     return (
        
         <>
-            "Hey !"<h1>{props.name}</h1>
+            <h1>Hey ! {name.name}</h1>
             <h2>Welcome to Newton School.</h2>
         </>
     );
