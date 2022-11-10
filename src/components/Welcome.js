dbac8b80040cbb5b39ca78c5235b78599868e196
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-10 19:04:46 +0530

    changed msg welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,11 +3,10 @@ import React from 'react';
 export default function Welcome(props){
     return (
        
-        <React.Fragment>
-            <h1>Hey !</h1>
-            <h1>{props.name}</h1>
+        <>
+            "Hey !"<h1>{props.name}</h1>
             <h2>Welcome to Newton School.</h2>
-        </React.Fragment>
+        </>
     );
     
 }
